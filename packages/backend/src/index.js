e212e44831124f159a0944efc626225d4206c3a1
ricardoparro <ricardo.parro@gmail.com>
2026-02-04 16:09:46 +0000

    fix(image): replace Paul Mazurkiewicz image URL (#209)
    Replace intermittently rate-limited Wikimedia image with a more reliable
    portrait photo from RockHard Festival 2016.
    
    Old URL: /8/8e/Cannibal_Corpse_Rockharz_2018_03.jpg (returning 429)
    New URL: /b/b9/20160515_Gelsenkirchen_RockHard_Festival_Cannibal_Corpse_0037_(cropped).jpg
    
    Closes #209

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1323,7 +1323,7 @@ const drummers = [
     genres: ['Death Metal'],
     country: 'USA',
     era: '80s',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Cannibal_Corpse_Rockharz_2018_03.jpg',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/20160515_Gelsenkirchen_RockHard_Festival_Cannibal_Corpse_0037_%28cropped%29.jpg',
     bio: 'Paul Mazurkiewicz (born September 8, 1968) is an American drummer and co-founder of legendary death metal band Cannibal Corpse, formed in Buffalo, New York in 1988. As one of the most consistent and dedicated drummers in extreme metal, Mazurkiewicz has performed on every Cannibal Corpse album since their 1990 debut "Eaten Back to Life." His drumming style combines relentless blast beats, complex double bass patterns, and groove-oriented sections that have helped define the death metal sound. With Cannibal Corpse being the best-selling death metal band of all time, Mazurkiewicz\'s contribution to the genre is immeasurable.',
     sameAs: [
       'https://en.wikipedia.org/wiki/Paul_Mazurkiewicz',
@@ -1337,7 +1337,7 @@ const drummers = [
       sticks: 'Vic Firth American Classic 5B'
     },
     photos: [
-      'https://upload.wikimedia.org/wikipedia/commons/8/8e/Cannibal_Corpse_Rockharz_2018_03.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/b/b9/20160515_Gelsenkirchen_RockHard_Festival_Cannibal_Corpse_0037_%28cropped%29.jpg'
     ],
     endorsements: [
       { name: 'Pearl Drums', url: 'https://pearldrum.com' },

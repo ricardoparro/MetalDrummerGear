81bf8139ce482ba2442627a5412697a370389fa1
Ricardo Parro <ricardo.parro@gmail.com>
2026-02-04 15:10:35 +0000

    fix: replace rate-limited drummer images (#206)
    Replace Wikimedia Commons images that were returning 429 errors:
    
    - Aquiles Priester: Changed from W.A.S.P. Wacken photo to El Salvador concert photo
    - Paul Mazurkiewicz: Changed from RockHard 2016 cropped to Rockharz 2018 photo
    
    Both replacement images are CC-licensed from Wikimedia Commons.
    
    Fixes #203

--- api/drummers/index.js
+++ api/drummers/index.js
@@ -2347,7 +2347,7 @@ const drummers = [
     band: 'Angra / W.A.S.P.',
     genre: 'Power Metal / Heavy Metal',
     country: 'Brazil',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/W.A.S.P._-_Wacken_Open_Air_-DAY_4_-8_%2854732202878%29_%28cropped%29.jpg',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Aquiles_Priester_Cropped_-Aquiles_Priester_El_SALVADOR.jpg',
     bio: 'Aquiles Priester (born 1971) is a Brazilian drummer renowned for his incredible speed, precision, and showmanship. He is best known for his work with Angra (2000-2012, 2023-present), W.A.S.P. (2006-present), and his own project Hangar. Priester\'s double bass abilities, complex fills, and ability to maintain blazing speeds for extended periods have made him one of the most celebrated power metal drummers in the world. He has won numerous awards from Brazilian and international music publications and is known for his energetic live performances.',
     sameAs: [
       'https://en.wikipedia.org/wiki/Aquiles_Priester',
@@ -2366,7 +2366,7 @@ const drummers = [
       sources: ['https://pearldrum.com/en/artists/', 'https://sabian.com/artists']
     },
     photos: [
-      'https://upload.wikimedia.org/wikipedia/commons/a/a7/Aquiles_Priester.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/a/a0/Aquiles_Priester_Cropped_-Aquiles_Priester_El_SALVADOR.jpg'
     ],
     endorsements: [
       { name: 'Pearl Drums', url: 'https://pearldrum.com' },
@@ -2390,7 +2390,7 @@ const drummers = [
     band: 'Cannibal Corpse',
     genre: 'Death Metal',
     country: 'USA',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/20160515_Gelsenkirchen_RockHard_Festival_Cannibal_Corpse_0037_%28cropped%29.jpg',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Cannibal_Corpse_Rockharz_2018_03.jpg',
     imageCredit: 'Photo by Markus Felix | PushingPixels, CC BY-SA 3.0, via Wikimedia Commons',
     bio: 'Paul Mazurkiewicz (born September 8, 1968) is an American drummer and co-founder of legendary death metal band Cannibal Corpse, formed in Buffalo, New York in 1988. As one of the most consistent and dedicated drummers in extreme metal, Mazurkiewicz has performed on every Cannibal Corpse album since their 1990 debut "Eaten Back to Life." His drumming style combines relentless blast beats, complex double bass patterns, and groove-oriented sections that have helped define the death metal sound. With Cannibal Corpse being the best-selling death metal band of all time, Mazurkiewicz\'s contribution to the genre is immeasurable.',
     sameAs: [

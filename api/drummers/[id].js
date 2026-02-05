6f9a08c10f19bc771a180a0b0f30099d78856f0a
ricardoparro <ricardo.parro@gmail.com>
2026-02-05 11:26:07 +0000

    fix: use CommonJS require for quotes-data import
    The api/drummers/[id].js file was using ESM import syntax to
    import from api/quotes-data.js which uses CommonJS exports.
    This module system mismatch caused the serverless function to
    fail silently, resulting in Vercel serving the SPA's index.html
    instead of JSON for /api/drummers/:id endpoints.
    
    Changed from:
      import { getQuotesForDrummer } from '../quotes-data.js'
      export default function handler
    
    To:
      const { getQuotesForDrummer } = require('../quotes-data.js')
      module.exports = function handler
    
    Fixes #269

--- api/drummers/[id].js
+++ api/drummers/[id].js
@@ -1,6 +1,6 @@
 // Vercel Serverless Function - Get drummer by ID
 
-import { getQuotesForDrummer } from '../quotes-data.js';
+const { getQuotesForDrummer } = require('../quotes-data.js');
 
 const drummers = [
   {
@@ -2128,7 +2128,7 @@ const drummers = [
   }
 ];
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=86400');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');

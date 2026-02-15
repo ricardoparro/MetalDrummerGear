36b0685a21cfa78fb76a3690a111922cf22dd758
Ricardo Parro <ricardo.parro@gmail.com>
2026-02-15 11:23:08 +0000

    feat(birthday): expand birthday data to 57 drummers (#343) (#420)
    - Add birth dates for all drummers in the database
    - Include living/deceased status
    - Add birth places for all drummers
    - Add helper function getMostPopularMonth()
    - Add getBirthdayStats() for fun facts section
    
    Closes #343

--- packages/frontend/data/birthdays.js
+++ packages/frontend/data/birthdays.js
@@ -3,6 +3,7 @@
 // Issue #343: Create drummer birthday calendar (shareable evergreen content)
 
 export const drummerBirthdays = [
+  // Metallica
   {
     slug: 'lars-ulrich',
     name: 'Lars Ulrich',
@@ -16,6 +17,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/lars-ulrich.webp',
     isLiving: true
   },
+  // Slipknot
   {
     slug: 'joey-jordison',
     name: 'Joey Jordison',
@@ -29,6 +31,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/joey-jordison.webp',
     isLiving: false
   },
+  // Testament / Death
   {
     slug: 'gene-hoglan',
     name: 'Gene Hoglan',
@@ -42,6 +45,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/gene-hoglan.webp',
     isLiving: true
   },
+  // Slayer
   {
     slug: 'dave-lombardo',
     name: 'Dave Lombardo',
@@ -55,6 +59,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/dave-lombardo.webp',
     isLiving: true
   },
+  // Meshuggah
   {
     slug: 'tomas-haake',
     name: 'Tomas Haake',
@@ -68,6 +73,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/tomas-haake.webp',
     isLiving: true
   },
+  // Nile
   {
     slug: 'george-kollias',
     name: 'George Kollias',
@@ -81,6 +87,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/george-kollias.webp',
     isLiving: true
   },
+  // Slipknot / Sepultura
   {
     slug: 'eloy-casagrande',
     name: 'Eloy Casagrande',
@@ -94,6 +101,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/eloy-casagrande.webp',
     isLiving: true
   },
+  // Dream Theater
   {
     slug: 'mike-portnoy',
     name: 'Mike Portnoy',
@@ -107,6 +115,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/mike-portnoy.webp',
     isLiving: true
   },
+  // Tool
   {
     slug: 'danny-carey',
     name: 'Danny Carey',
@@ -120,6 +129,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/danny-carey.webp',
     isLiving: true
   },
+  // Gojira
   {
     slug: 'mario-duplantier',
     name: 'Mario Duplantier',
@@ -133,6 +143,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/mario-duplantier.webp',
     isLiving: true
   },
+  // Slayer
   {
     slug: 'paul-bostaph',
     name: 'Paul Bostaph',
@@ -146,6 +157,7 @@ export const drummerBirthdays = [
     image: '/images/drummers/paul-bostaph.webp',
     isLiving: true
   },
+  // Testament / Slayer
   {
     slug: 'jon-dette',
     name: 'Jon Dette',
@@ -157,6 +169,636 @@ export const drummerBirthdays = [
     birthPlace: 'Milwaukee, Wisconsin, USA',
     deathDate: null,
     image: '/images/drummers/jon-dette.webp',
+    isLiving: true
+  },
+  // Iron Maiden
+  {
+    slug: 'nicko-mcbrain',
+    name: 'Nicko McBrain',
+    band: 'Iron Maiden',
+    birthDate: '1952-06-05',
+    birthMonth: 6,
+    birthDay: 5,
+    birthYear: 1952,
+    birthPlace: 'Hackney, London, UK',
+    deathDate: null,
+    image: '/images/drummers/nicko-mcbrain.webp',
+    isLiving: true
+  },
+  // Judas Priest
+  {
+    slug: 'scott-travis',
+    name: 'Scott Travis',
+    band: 'Judas Priest',
+    birthDate: '1961-09-06',
+    birthMonth: 9,
+    birthDay: 6,
+    birthYear: 1961,
+    birthPlace: 'Norfolk, Virginia, USA',
+    deathDate: null,
+    image: '/images/drummers/scott-travis.webp',
+    isLiving: true
+  },
+  // Motörhead / Scorpions
+  {
+    slug: 'mikkey-dee',
+    name: 'Mikkey Dee',
+    band: 'Motörhead / Scorpions',
+    birthDate: '1963-10-31',
+    birthMonth: 10,
+    birthDay: 31,
+    birthYear: 1963,
+    birthPlace: 'Gothenburg, Sweden',
+    deathDate: null,
+    image: '/images/drummers/mikkey-dee.webp',
+    isLiving: true
+  },
+  // Black Sabbath
+  {
+    slug: 'bill-ward',
+    name: 'Bill Ward',
+    band: 'Black Sabbath',
+    birthDate: '1948-05-05',
+    birthMonth: 5,
+    birthDay: 5,
+    birthYear: 1948,
+    birthPlace: 'Aston, Birmingham, UK',
+    deathDate: null,
+    image: '/images/drummers/bill-ward.webp',
+    isLiving: true
+  },
+  // Pantera / Hellyeah
+  {
+    slug: 'vinnie-paul',
+    name: 'Vinnie Paul',
+    band: 'Pantera / Hellyeah',
+    birthDate: '1964-03-11',
+    birthMonth: 3,
+    birthDay: 11,
+    birthYear: 1964,
+    birthPlace: 'Arlington, Texas, USA',
+    deathDate: '2018-06-22',
+    image: '/images/drummers/vinnie-paul.webp',
+    isLiving: false
+  },
+  // Anthrax
+  {
+    slug: 'charlie-benante',
+    name: 'Charlie Benante',
+    band: 'Anthrax',
+    birthDate: '1962-11-27',
+    birthMonth: 11,
+    birthDay: 27,
+    birthYear: 1962,
+    birthPlace: 'The Bronx, New York, USA',
+    deathDate: null,
+    image: '/images/drummers/charlie-benante.webp',
+    isLiving: true
+  },
+  // Sepultura
+  {
+    slug: 'igor-cavalera',
+    name: 'Igor Cavalera',
+    band: 'Sepultura',
+    birthDate: '1970-09-04',
+    birthMonth: 9,
+    birthDay: 4,
+    birthYear: 1970,
+    birthPlace: 'Belo Horizonte, Brazil',
+    deathDate: null,
+    image: '/images/drummers/igor-cavalera.webp',
+    isLiving: true
+  },
+  // Cryptopsy
+  {
+    slug: 'flo-mounier',
+    name: 'Flo Mounier',
+    band: 'Cryptopsy',
+    birthDate: '1974-05-06',
+    birthMonth: 5,
+    birthDay: 6,
+    birthYear: 1974,
+    birthPlace: 'Montreal, Quebec, Canada',
+    deathDate: null,
+    image: '/images/drummers/flo-mounier.webp',
+    isLiving: true
+  },
+  // Shadows Fall / Overkill
+  {
+    slug: 'jason-bittner',
+    name: 'Jason Bittner',
+    band: 'Shadows Fall / Overkill',
+    birthDate: '1970-04-24',
+    birthMonth: 4,
+    birthDay: 24,
+    birthYear: 1970,
+    birthPlace: 'Long Island, New York, USA',
+    deathDate: null,
+    image: '/images/drummers/jason-bittner.webp',
+    isLiving: true
+  },
+  // Opeth / Soen
+  {
+    slug: 'martin-lopez',
+    name: 'Martin Lopez',
+    band: 'Opeth / Soen',
+    birthDate: '1978-06-01',
+    birthMonth: 6,
+    birthDay: 1,
+    birthYear: 1978,
+    birthPlace: 'Oviedo, Spain',
+    deathDate: null,
+    image: '/images/drummers/martin-lopez.webp',
+    isLiving: true
+  },
+  // Periphery
+  {
+    slug: 'travis-orbin',
+    name: 'Travis Orbin',
+    band: 'Periphery',
+    birthDate: '1983-09-25',
+    birthMonth: 9,
+    birthDay: 25,
+    birthYear: 1983,
+    birthPlace: 'Baltimore, Maryland, USA',
+    deathDate: null,
+    image: '/images/drummers/travis-orbin.webp',
+    isLiving: true
+  },
+  // Devin Townsend Project
+  {
+    slug: 'ryan-van-poederooyen',
+    name: 'Ryan Van Poederooyen',
+    band: 'Devin Townsend Project',
+    birthDate: '1971-12-20',
+    birthMonth: 12,
+    birthDay: 20,
+    birthYear: 1971,
+    birthPlace: 'Vancouver, Canada',
+    deathDate: null,
+    image: '/images/drummers/ryan-van-poederooyen.webp',
+    isLiving: true
+  },
+  // Godsmack
+  {
+    slug: 'shannon-larkin',
+    name: 'Shannon Larkin',
+    band: 'Godsmack',
+    birthDate: '1967-04-24',
+    birthMonth: 4,
+    birthDay: 24,
+    birthYear: 1967,
+    birthPlace: 'Baltimore, Maryland, USA',
+    deathDate: null,
+    image: '/images/drummers/shannon-larkin.webp',
+    isLiving: true
+  },
+  // Fear Factory
+  {
+    slug: 'raymond-herrera',
+    name: 'Raymond Herrera',
+    band: 'Fear Factory',
+    birthDate: '1972-01-18',
+    birthMonth: 1,
+    birthDay: 18,
+    birthYear: 1972,
+    birthPlace: 'Los Angeles, California, USA',
+    deathDate: null,
+    image: '/images/drummers/raymond-herrera.webp',
+    isLiving: true
+  },
+  // Mats/Morgan Band
+  {
+    slug: 'morgan-agren',
+    name: 'Morgan Ågren',
+    band: 'Mats/Morgan Band',
+    birthDate: '1967-08-12',
+    birthMonth: 8,
+    birthDay: 12,
+    birthYear: 1967,
+    birthPlace: 'Umeå, Sweden',
+    deathDate: null,
+    image: '/images/drummers/morgan-agren.webp',
+    isLiving: true
+  },
+  // Trivium
+  {
+    slug: 'nick-augusto',
+    name: 'Nick Augusto',
+    band: 'Trivium',
+    birthDate: '1987-05-03',
+    birthMonth: 5,
+    birthDay: 3,
+    birthYear: 1987,
+    birthPlace: 'Orlando, Florida, USA',
+    deathDate: null,
+    image: '/images/drummers/nick-augusto.webp',
+    isLiving: true
+  },
+  // Oceans Ate Alaska
+  {
+    slug: 'chris-turner',
+    name: 'Chris Turner',
+    band: 'Oceans Ate Alaska',
+    birthDate: '1992-02-04',
+    birthMonth: 2,
+    birthDay: 4,
+    birthYear: 1992,
+    birthPlace: 'Birmingham, UK',
+    deathDate: null,
+    image: '/images/drummers/chris-turner.webp',
+    isLiving: true
+  },
+  // August Burns Red
+  {
+    slug: 'matt-greiner',
+    name: 'Matt Greiner',
+    band: 'August Burns Red',
+    birthDate: '1987-11-19',
+    birthMonth: 11,
+    birthDay: 19,
+    birthYear: 1987,
+    birthPlace: 'Lancaster, Pennsylvania, USA',
+    deathDate: null,
+    image: '/images/drummers/matt-greiner.webp',
+    isLiving: true
+  },
+  // Between the Buried and Me
+  {
+    slug: 'blake-richardson',
+    name: 'Blake Richardson',
+    band: 'Between the Buried and Me',
+    birthDate: '1983-06-05',
+    birthMonth: 6,
+    birthDay: 5,
+    birthYear: 1983,
+    birthPlace: 'North Carolina, USA',
+    deathDate: null,
+    image: '/images/drummers/blake-richardson.webp',
+    isLiving: true
+  },
+  // Converge / Mutoid Man
+  {
+    slug: 'ben-koller',
+    name: 'Ben Koller',
+    band: 'Converge / Mutoid Man',
+    birthDate: '1977-08-27',
+    birthMonth: 8,
+    birthDay: 27,
+    birthYear: 1977,
+    birthPlace: 'Massachusetts, USA',
+    deathDate: null,
+    image: '/images/drummers/ben-koller.webp',
+    isLiving: true
+  },
+  // Cannibal Corpse
+  {
+    slug: 'paul-mazurkiewicz',
+    name: 'Paul Mazurkiewicz',
+    band: 'Cannibal Corpse',
+    birthDate: '1968-09-08',
+    birthMonth: 9,
+    birthDay: 8,
+    birthYear: 1968,
+    birthPlace: 'Buffalo, New York, USA',
+    deathDate: null,
+    image: '/images/drummers/paul-mazurkiewicz.webp',
+    isLiving: true
+  },
+  // Dream Theater
+  {
+    slug: 'mike-mangini',
+    name: 'Mike Mangini',
+    band: 'Dream Theater',
+    birthDate: '1963-04-18',
+    birthMonth: 4,
+    birthDay: 18,
+    birthYear: 1963,
+    birthPlace: 'Newton, Massachusetts, USA',
+    deathDate: null,
+    image: '/images/drummers/mike-mangini.webp',
+    isLiving: true
+  },
+  // Animals as Leaders
+  {
+    slug: 'matt-garstka',
+    name: 'Matt Garstka',
+    band: 'Animals as Leaders',
+    birthDate: '1988-11-15',
+    birthMonth: 11,
+    birthDay: 15,
+    birthYear: 1988,
+    birthPlace: 'Westfield, Massachusetts, USA',
+    deathDate: null,
+    image: '/images/drummers/matt-garstka.webp',
+    isLiving: true
+  },
+  // Arch Enemy
+  {
+    slug: 'daniel-erlandsson',
+    name: 'Daniel Erlandsson',
+    band: 'Arch Enemy',
+    birthDate: '1974-10-21',
+    birthMonth: 10,
+    birthDay: 21,
+    birthYear: 1974,
+    birthPlace: 'Malmö, Sweden',
+    deathDate: null,
+    image: '/images/drummers/daniel-erlandsson.webp',
+    isLiving: true
+  },
+  // Children of Bodom
+  {
+    slug: 'jaska-raatikainen',
+    name: 'Jaska Raatikainen',
+    band: 'Children of Bodom',
+    birthDate: '1979-07-14',
+    birthMonth: 7,
+    birthDay: 14,
+    birthYear: 1979,
+    birthPlace: 'Espoo, Finland',
+    deathDate: null,
+    image: '/images/drummers/jaska-raatikainen.webp',
+    isLiving: true
+  },
+  // Obscura / Alkaloid
+  {
+    slug: 'hannes-grossmann',
+    name: 'Hannes Grossmann',
+    band: 'Obscura / Alkaloid',
+    birthDate: '1982-07-25',
+    birthMonth: 7,
+    birthDay: 25,
+    birthYear: 1982,
+    birthPlace: 'Regensburg, Germany',
+    deathDate: null,
+    image: '/images/drummers/hannes-grossmann.webp',
+    isLiving: true
+  },
+  // Behemoth / Vader
+  {
+    slug: 'daray',
+    name: 'Daray',
+    band: 'Behemoth / Vader',
+    birthDate: '1979-06-06',
+    birthMonth: 6,
+    birthDay: 6,
+    birthYear: 1979,
+    birthPlace: 'Białystok, Poland',
+    deathDate: null,
+    image: '/images/drummers/daray.webp',
+    isLiving: true
+  },
+  // Amon Amarth
+  {
+    slug: 'jocke-wallgren',
+    name: 'Jocke Wallgren',
+    band: 'Amon Amarth',
+    birthDate: '1981-11-04',
+    birthMonth: 11,
+    birthDay: 4,
+    birthYear: 1981,
+    birthPlace: 'Stockholm, Sweden',
+    deathDate: null,
+    image: '/images/drummers/jocke-wallgren.webp',
+    isLiving: true
+  },
+  // Vital Remains / Hate Eternal
+  {
+    slug: 'tim-yeung',
+    name: 'Tim Yeung',
+    band: 'Vital Remains / Hate Eternal',
+    birthDate: '1975-02-21',
+    birthMonth: 2,
+    birthDay: 21,
+    birthYear: 1975,
+    birthPlace: 'Allentown, Pennsylvania, USA',
+    deathDate: null,
+    image: '/images/drummers/tim-yeung.webp',
+    isLiving: true
+  },
+  // Suffocation / Six Feet Under
+  {
+    slug: 'kevin-talley',
+    name: 'Kevin Talley',
+    band: 'Suffocation / Six Feet Under',
+    birthDate: '1977-03-13',
+    birthMonth: 3,
+    birthDay: 13,
+    birthYear: 1977,
+    birthPlace: 'Springfield, Virginia, USA',
+    deathDate: null,
+    image: '/images/drummers/kevin-talley.webp',
+    isLiving: true
+  },
+  // Nile / Hate Eternal
+  {
+    slug: 'derek-roddy',
+    name: 'Derek Roddy',
+    band: 'Nile / Hate Eternal',
+    birthDate: '1972-01-28',
+    birthMonth: 1,
+    birthDay: 28,
+    birthYear: 1972,
+    birthPlace: 'Columbia, South Carolina, USA',
+    deathDate: null,
+    image: '/images/drummers/derek-roddy.webp',
+    isLiving: true
+  },
+  // Soilwork / Megadeth
+  {
+    slug: 'dirk-verbeuren',
+    name: 'Dirk Verbeuren',
+    band: 'Soilwork / Megadeth',
+    birthDate: '1975-01-08',
+    birthMonth: 1,
+    birthDay: 8,
+    birthYear: 1975,
+    birthPlace: 'Lommel, Belgium',
+    deathDate: null,
+    image: '/images/drummers/dirk-verbeuren.webp',
+    isLiving: true
+  },
+  // Satyricon / 1349
+  {
+    slug: 'frost',
+    name: 'Frost',
+    band: 'Satyricon / 1349',
+    birthDate: '1973-06-28',
+    birthMonth: 6,
+    birthDay: 28,
+    birthYear: 1973,
+    birthPlace: 'Oslo, Norway',
+    deathDate: null,
+    image: '/images/drummers/frost.webp',
+    isLiving: true
+  },
+  // Porcupine Tree / King Crimson
+  {
+    slug: 'gavin-harrison',
+    name: 'Gavin Harrison',
+    band: 'Porcupine Tree / King Crimson',
+    birthDate: '1963-05-28',
+    birthMonth: 5,
+    birthDay: 28,
+    birthYear: 1963,
+    birthPlace: 'Harrow, London, UK',
+    deathDate: null,
+    image: '/images/drummers/gavin-harrison.webp',
+    isLiving: true
+  },
+  // Deftones
+  {
+    slug: 'abe-cunningham',
+    name: 'Abe Cunningham',
+    band: 'Deftones',
+    birthDate: '1973-07-27',
+    birthMonth: 7,
+    birthDay: 27,
+    birthYear: 1973,
+    birthPlace: 'Sacramento, California, USA',
+    deathDate: null,
+    image: '/images/drummers/abe-cunningham.webp',
+    isLiving: true
+  },
+  // Death / Iced Earth
+  {
+    slug: 'richard-christy',
+    name: 'Richard Christy',
+    band: 'Death / Iced Earth',
+    birthDate: '1974-02-01',
+    birthMonth: 2,
+    birthDay: 1,
+    birthYear: 1974,
+    birthPlace: 'Fort Scott, Kansas, USA',
+    deathDate: null,
+    image: '/images/drummers/richard-christy.webp',
+    isLiving: true
+  },
+  // Angra / Primal Fear
+  {
+    slug: 'aquiles-priester',
+    name: 'Aquiles Priester',
+    band: 'Angra / Primal Fear',
+    birthDate: '1971-09-17',
+    birthMonth: 9,
+    birthDay: 17,
+    birthYear: 1971,
+    birthPlace: 'Porto Alegre, Brazil',
+    deathDate: null,
+    image: '/images/drummers/aquiles-priester.webp',
+    isLiving: true
+  },
+  // Mastodon
+  {
+    slug: 'brann-dailor',
+    name: 'Brann Dailor',
+    band: 'Mastodon',
+    birthDate: '1975-03-19',
+    birthMonth: 3,
+    birthDay: 19,
+    birthYear: 1975,
+    birthPlace: 'Rochester, New York, USA',
+    deathDate: null,
+    image: '/images/drummers/brann-dailor.webp',
+    isLiving: true
+  },
+  // Lamb of God
+  {
+    slug: 'chris-adler',
+    name: 'Chris Adler',
+    band: 'Lamb of God',
+    birthDate: '1972-11-23',
+    birthMonth: 11,
+    birthDay: 23,
+    birthYear: 1972,
+    birthPlace: 'Richmond, Virginia, USA',
+    deathDate: null,
+    image: '/images/drummers/chris-adler.webp',
+    isLiving: true
+  },
+  // Periphery
+  {
+    slug: 'matt-halpern',
+    name: 'Matt Halpern',
+    band: 'Periphery',
+    birthDate: '1984-02-14',
+    birthMonth: 2,
+    birthDay: 14,
+    birthYear: 1984,
+    birthPlace: 'Baltimore, Maryland, USA',
+    deathDate: null,
+    image: '/images/drummers/matt-halpern.webp',
+    isLiving: true
+  },
+  // Behemoth
+  {
+    slug: 'inferno',
+    name: 'Inferno',
+    band: 'Behemoth',
+    birthDate: '1979-03-30',
+    birthMonth: 3,
+    birthDay: 30,
+    birthYear: 1979,
+    birthPlace: 'Gdańsk, Poland',
+    deathDate: null,
+    image: '/images/drummers/inferno.webp',
+    isLiving: true
+  },
+  // Mayhem / Dimmu Borgir
+  {
+    slug: 'hellhammer',
+    name: 'Hellhammer',
+    band: 'Mayhem / Dimmu Borgir',
+    birthDate: '1971-02-06',
+    birthMonth: 2,
+    birthDay: 6,
+    birthYear: 1971,
+    birthPlace: 'Oslo, Norway',
+    deathDate: null,
+    image: '/images/drummers/hellhammer.webp',
+    isLiving: true
+  },
+  // Korn
+  {
+    slug: 'ray-luzier',
+    name: 'Ray Luzier',
+    band: 'Korn',
+    birthDate: '1970-06-14',
+    birthMonth: 6,
+    birthDay: 14,
+    birthYear: 1970,
+    birthPlace: 'Pittsburgh, Pennsylvania, USA',
+    deathDate: null,
+    image: '/images/drummers/ray-luzier.webp',
+    isLiving: true
+  },
+  // Limp Bizkit
+  {
+    slug: 'john-otto',
+    name: 'John Otto',
+    band: 'Limp Bizkit',
+    birthDate: '1977-03-22',
+    birthMonth: 3,
+    birthDay: 22,
+    birthYear: 1977,
+    birthPlace: 'Jacksonville, Florida, USA',
+    deathDate: null,
+    image: '/images/drummers/john-otto.webp',
+    isLiving: true
+  },
+  // Slipknot
+  {
+    slug: 'jay-weinberg',
+    name: 'Jay Weinberg',
+    band: 'Slipknot',
+    birthDate: '1990-09-08',
+    birthMonth: 9,
+    birthDay: 8,
+    birthYear: 1990,
+    birthPlace: 'New Jersey, USA',
+    deathDate: null,
+    image: '/images/drummers/jay-weinberg.webp',
     isLiving: true
   }
 ];
@@ -285,6 +927,41 @@ export function getZodiacSign(month, day) {
   }
   
   return signs[0]; // Default to Capricorn
+}
+
+// Get month with most birthdays
+export function getMostPopularMonth() {
+  const counts = {};
+  for (let month = 1; month <= 12; month++) {
+    counts[month] = getBirthdaysByMonth(month).length;
+  }
+  const maxMonth = Object.entries(counts).reduce((a, b) => a[1] > b[1] ? a : b)[0];
+  return MONTH_NAMES[parseInt(maxMonth) - 1];
+}
+
+// Get birthday stats
+export function getBirthdayStats() {
+  const living = drummerBirthdays.filter(d => d.isLiving).length;
+  const total = drummerBirthdays.length;
+  const avgAge = Math.round(
+    drummerBirthdays
+      .filter(d => d.isLiving)
+      .reduce((sum, d) => sum + calculateAge(d.birthYear, d.birthMonth, d.birthDay), 0) / living
+  );
+  
+  return {
+    total,
+    living,
+    deceased: total - living,
+    avgAge,
+    oldestDrummer: drummerBirthdays.filter(d => d.isLiving).reduce((oldest, d) => 
+      d.birthYear < oldest.birthYear ? d : oldest
+    ),
+    youngestDrummer: drummerBirthdays.filter(d => d.isLiving).reduce((youngest, d) => 
+      d.birthYear > youngest.birthYear ? d : youngest
+    ),
+    mostPopularMonth: getMostPopularMonth()
+  };
 }
 
 // Export for convenience

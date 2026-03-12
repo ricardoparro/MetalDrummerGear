65a74721e6981e28dc545a7f0de1aac34ec6fd49
ricardoparro <ricardo.parro@gmail.com>
2026-03-12 13:40:07 +0000

    debug: disable CSS fix to test article page rendering

--- packages/frontend/fixCssStyles.js
+++ packages/frontend/fixCssStyles.js
@@ -1,45 +1,10 @@
 /**
- * CSS Fix - Proxy approach (works for non-input pages)
+ * CSS Fix - TEMPORARILY DISABLED FOR DEBUGGING
  * 
- * This fixes the Safari/WebKit CSS bug where react-native-web
- * tries to set numeric properties on CSSStyleDeclaration.
- * 
- * Note: This Proxy interferes with input focus, so search is
- * temporarily disabled until we find a better solution.
+ * The Proxy approach was causing pages to not render.
+ * Disabling to identify if this is the root cause.
  */
 
-if (typeof window !== 'undefined') {
-  const proxyCache = new WeakMap();
-  
-  const originalStyleGetter = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'style');
-  if (originalStyleGetter && originalStyleGetter.get) {
-    Object.defineProperty(HTMLElement.prototype, 'style', {
-      get: function() {
-        const realStyle = originalStyleGetter.get.call(this);
-        
-        let proxy = proxyCache.get(realStyle);
-        if (proxy) return proxy;
-        
-        proxy = new Proxy(realStyle, {
-          set(target, prop, value) {
-            if (typeof prop === 'string' && /^\d+$/.test(prop)) {
-              return true;
-            }
-            target[prop] = value;
-            return true;
-          },
-          get(target, prop) {
-            const val = target[prop];
-            return typeof val === 'function' ? val.bind(target) : val;
-          }
-        });
-        
-        proxyCache.set(realStyle, proxy);
-        return proxy;
-      },
-      configurable: true,
-    });
-  }
-}
+// TODO: Find a fix that works for both CSS and page rendering
 
 export default {};

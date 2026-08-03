3e6c52f064c1b016c4da90846f3ffc6ab6bc41ad
Ricardo Parro <ricardo.parro@gmail.com>
2026-08-03 08:29:05 +0000

    fix: #5200 add ssrLinks to 12 gear reference sub-pages back to family hubs

--- api/meta/[...path].js
+++ api/meta/[...path].js
@@ -6699,6 +6699,7 @@ export function getMetaForPath(pathname) {
       // brand/reference/setup sub-routes, unlike drummer profiles, articles, etc.
       speakableSchema: true,
       speakableCssSelector: ['h1', 'h2', 'p'],
+      ssrLinks: [{ href: '/drumsticks', label: 'Drumsticks' }],
     };
   }
 
@@ -6898,6 +6899,7 @@ export function getMetaForPath(pathname) {
       // brand/reference/setup sub-routes, unlike drummer profiles, articles, etc.
       speakableSchema: true,
       speakableCssSelector: ['h1', 'h2', 'p'],
+      ssrLinks: [{ href: '/cymbals', label: 'Cymbals' }],
     };
   }
 
@@ -7090,6 +7092,7 @@ export function getMetaForPath(pathname) {
       // brand/reference/setup sub-routes, unlike drummer profiles, articles, etc.
       speakableSchema: true,
       speakableCssSelector: ['h1', 'h2', 'p'],
+      ssrLinks: [{ href: '/snares', label: 'Snares' }],
     };
   }
 
@@ -7289,6 +7292,7 @@ export function getMetaForPath(pathname) {
       // brand/reference/setup sub-routes, unlike drummer profiles, articles, etc.
       speakableSchema: true,
       speakableCssSelector: ['h1', 'h2', 'p'],
+      ssrLinks: [{ href: '/pedals', label: 'Pedals' }],
     };
   }
 
